097112316dcd158905d35903afa3a69b1f591165
b-rich-dev <eugen.birich@gmail.com>
2025-05-20 08:11:45 +0200

    clean code

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function init() {
     getQuizLenght()
-    showCurrentQuestions()
+    showQuestions()
 }
 
 
@@ -9,41 +9,59 @@ function getQuizLenght() {
 }
 
 
-function showCurrentQuestions() {
+function showQuestions() {
 
-    if (currentQuestion >= questions.length) {
-        document.getElementById("end-quiz").style = "";
-        document.getElementById("quiz").style = "display: none;";
-        document.getElementById("amount-of-questions").innerHTML = questions.length;
-        document.getElementById("amount-of-right-questions").innerHTML = rightQuestions;
-        document.getElementById("tropy").style = "";
+    if (gameIsOver()) {
+        showEndScreen();
     } else {
-        let percent = (currentQuestion + 1) / questions.length;
-        percent = Math.round(percent * 100);
-
-        document.getElementById("progress-bar").textContent = `${percent}%`;
-        document.getElementById("progress-bar").style.width = `${percent}%`;
-        document.getElementById("progress-bar").setAttribute("aria-valuenow", `${percent}`);
-
-        let question = questions[currentQuestion];
-
-        if (!question) {
-            console.warn("Keine Frage für Index:", currentQuestion);
-            return;
-        }
-
-        document.getElementById("question_text").innerHTML = question['question']
-        document.getElementById("answer_a").innerHTML = question['answerA']
-        document.getElementById("answer_b").innerHTML = question['answerB']
-        document.getElementById("answer_c").innerHTML = question['answerC']
-        document.getElementById("answer_d").innerHTML = question['answerD']
-
-        document.getElementById("question-number").innerHTML = currentQuestion + 1
-
-        document.getElementById("previous-button").disabled = currentQuestion === 0;
-        // document.getElementById("next-button").disabled = currentQuestion >= questions.length - 1;
-        document.getElementById("next-button").disabled = true;
+        updateProgressBar();
+        updateToNextQuestion();
     }
+}
+
+
+function gameIsOver() {
+    return currentQuestion >= questions.length
+}
+
+
+function updateProgressBar() {
+    let percent = (currentQuestion + 1) / questions.length;
+    percent = Math.round(percent * 100);
+
+    document.getElementById("progress-bar").textContent = `${percent}%`;
+    document.getElementById("progress-bar").style.width = `${percent}%`;
+    document.getElementById("progress-bar").setAttribute("aria-valuenow", `${percent}`);
+}
+
+
+function updateToNextQuestion() {
+    let question = questions[currentQuestion];
+
+    if (!question) {
+        console.warn("Keine Frage für Index:", currentQuestion);
+        return;
+    }
+
+    document.getElementById("question_text").innerHTML = question['question']
+    document.getElementById("answer_a").innerHTML = question['answerA']
+    document.getElementById("answer_b").innerHTML = question['answerB']
+    document.getElementById("answer_c").innerHTML = question['answerC']
+    document.getElementById("answer_d").innerHTML = question['answerD']
+
+    document.getElementById("question-number").innerHTML = currentQuestion + 1
+
+    document.getElementById("previous-button").disabled = currentQuestion === 0;
+    document.getElementById("next-button").disabled = true;
+}
+
+
+function showEndScreen() {
+    document.getElementById("end-quiz").style = "";
+    document.getElementById("quiz").style = "display: none;";
+    document.getElementById("amount-of-questions").innerHTML = questions.length;
+    document.getElementById("amount-of-right-questions").innerHTML = rightQuestions;
+    document.getElementById("tropy").style = "";
 }
 
 
@@ -54,7 +72,7 @@ function answer(selection) {
     let idLetter = `letter_${selectedQuestionCharacter.toLowerCase()}`;
     let correctLetter = `letter_${question['correctAnswer'].toLowerCase()}`;
 
-    if (selectedQuestionCharacter == question['correctAnswer']) {
+    if (rightAnswerSelected(selectedQuestionCharacter, question)) {
         returnGreenClass(selection, idLetter);
         rightQuestions++;
         AUDIO_SUCCESS.play();
@@ -64,6 +82,11 @@ function answer(selection) {
         AUDIO_FAIL.play();
     }
     document.getElementById("next-button").disabled = false;
+}
+
+
+function rightAnswerSelected(selectedQuestionCharacter, question) {
+    return selectedQuestionCharacter == question['correctAnswer']
 }
 
 
@@ -87,16 +110,16 @@ function nextQuestion() {
     currentQuestion++;
     document.getElementById("next-button").disabled = true;
     resetAnswerButtons()
-    showCurrentQuestions()
+    showQuestions()
 }
 
 
 function previousQuestion() {
-    
+
     if (currentQuestion > 0) {
         document.getElementById("previous-button").disabled = false;
         currentQuestion--;
-        showCurrentQuestions();
+        showQuestions();
     }
 
     if (currentQuestion === 0) {
@@ -114,7 +137,7 @@ function showRightAnswer(idOfRightAnswer, correctLetter) {
     let question = questions[currentQuestion];
     idOfRightAnswer = `answer_${question['correctAnswer'].toLowerCase()}`;
     correctLetter = `letter_${question['correctAnswer'].toLowerCase()}`;
-    
+
     document.getElementById(idOfRightAnswer).parentNode.classList.add('background-light-green')
     document.getElementById(idOfRightAnswer).classList.add('background-light-green')
     document.getElementById(correctLetter).classList.add('bg-success')
@@ -123,39 +146,18 @@ function showRightAnswer(idOfRightAnswer, correctLetter) {
 
 
 function resetAnswerButtons() {
-    document.getElementById("answer_a").parentNode.classList.remove('background-light-green')
-    document.getElementById("answer_a").classList.remove('background-light-green')
-    document.getElementById("answer_a").parentNode.classList.remove('background-light-red')
-    document.getElementById("answer_a").classList.remove('background-light-red')
+    const options = ['a', 'b', 'c', 'd'];
 
-    document.getElementById("letter_a").classList.remove('bg-success')
-    document.getElementById("letter_a").classList.remove('bg-danger')
+    options.forEach(option => {
+        const answerElement = document.getElementById(`answer_${option}`);
+        const letterElement = document.getElementById(`letter_${option}`);
 
-    document.getElementById("answer_b").parentNode.classList.remove('background-light-green')
-    document.getElementById("answer_b").classList.remove('background-light-green')
-    document.getElementById("answer_b").parentNode.classList.remove('background-light-red')
-    document.getElementById("answer_b").classList.remove('background-light-red')
+        answerElement.parentNode.classList.remove('background-light-green', 'background-light-red');
+        answerElement.classList.remove('background-light-green', 'background-light-red');
 
-    document.getElementById("letter_b").classList.remove('bg-success')
-    document.getElementById("letter_b").classList.remove('bg-danger')
-
-    document.getElementById("answer_c").parentNode.classList.remove('background-light-green')
-    document.getElementById("answer_c").classList.remove('background-light-green')
-    document.getElementById("answer_c").parentNode.classList.remove('background-light-red')
-    document.getElementById("answer_c").classList.remove('background-light-red')
-
-    document.getElementById("letter_c").classList.remove('bg-success')
-    document.getElementById("letter_c").classList.remove('bg-danger')
-
-    document.getElementById("answer_d").parentNode.classList.remove('background-light-green')
-    document.getElementById("answer_d").classList.remove('background-light-green')
-    document.getElementById("answer_d").parentNode.classList.remove('background-light-red')
-    document.getElementById("answer_d").classList.remove('background-light-red')
-
-    document.getElementById("letter_d").classList.remove('bg-success')
-    document.getElementById("letter_d").classList.remove('bg-danger')
+        letterElement.classList.remove('bg-success', 'bg-danger');
+    });
 }
-
 
 function restartGame() {
     document.getElementById("end-quiz").style = "display: none;";
